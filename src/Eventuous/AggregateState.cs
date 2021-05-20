c9c6da2f71596e3f058c28d069051130cc601980
Alexey Zimarev <alex@zimarev.com>
2021-05-20 09:59:24 +0200

    Added LoadState to the same interface (for now)

--- src/Eventuous/AggregateState.cs
+++ src/Eventuous/AggregateState.cs
@@ -1,8 +1,26 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
 namespace Eventuous {
     public abstract record AggregateState<T> where T : AggregateState<T> {
-        public abstract T When(object @event);
+        public virtual T When(object @event) {
+            var eventType = @event.GetType();
+            if (!_handlers.ContainsKey(eventType)) return (T) this;
+
+            return _handlers[eventType](@event);
+        }
+
+        [PublicAPI]
+        protected void On<TEvent>(Func<TEvent, T> handle) {
+            if (!_handlers.TryAdd(typeof(TEvent), x => handle((TEvent) x))) {
+                throw new InvalidOperationException($"Duplicate handler for {typeof(TEvent).Name}");
+            }
+        }
+
+        readonly Dictionary<Type, Func<object, T>> _handlers = new();
     }
-    
+
     public abstract record AggregateState<T, TId> : AggregateState<T>
         where T : AggregateState<T, TId>
         where TId : AggregateId {
--- src/Eventuous/IAggregateStore.cs
+++ src/Eventuous/IAggregateStore.cs
@@ -7,5 +7,8 @@ namespace Eventuous
         Task Store<T>(T entity, CancellationToken cancellationToken) where T : Aggregate;
 
         Task<T> Load<T>(string id, CancellationToken cancellationToken) where T : Aggregate, new();
+
+        Task<T> LoadState<T, TId>(StreamName stream, CancellationToken cancellationToken)
+            where T : AggregateState<T, TId>, new() where TId : AggregateId;
     }
 }
