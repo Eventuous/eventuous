d017f2e57043fbe1bd5a80d975b0fb1578f0401a
Alexey Zimarev <alex@zimarev.com>
2021-11-03 15:09:08 +0100

    Added a simple retry handling with Polly for #7

---
+++ src/Extensions/src/Eventuous.Subscriptions.Polly/PollyEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Eventuous.Subscriptions.Context;
+using Polly;
+
+namespace Eventuous.Subscriptions.Polly;
+
+/// <summary>
+/// Wrapping handler to execute the inner handler with a given retry policy
+/// </summary>
+public class PollyEventHandler : IEventHandler {
+    readonly IEventHandler _inner;
+    readonly IAsyncPolicy  _retryPolicy;
+
+    public PollyEventHandler(IEventHandler inner, IAsyncPolicy retryPolicy) {
+        _inner       = inner;
+        _retryPolicy = retryPolicy;
+    }
+
+    public Task HandleEvent(
+        IMessageConsumeContext context,
+        CancellationToken      cancellationToken
+    )
+        => _retryPolicy.ExecuteAsync(() => _inner.HandleEvent(context, cancellationToken));
+}
