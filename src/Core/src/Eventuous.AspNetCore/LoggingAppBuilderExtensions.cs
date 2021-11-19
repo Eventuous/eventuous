60fdb4e46190ee32a44655aa331c349b58cc8357
Alexey Zimarev <alex@zimarev.com>
2021-11-19 11:53:27 +0100

    Remove using

--- src/Core/src/Eventuous.AspNetCore/LoggingAppBuilderExtensions.cs
+++ src/Core/src/Eventuous.AspNetCore/LoggingAppBuilderExtensions.cs
@@ -1,5 +1,4 @@
 using Eventuous.Diagnostics.Logging;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
