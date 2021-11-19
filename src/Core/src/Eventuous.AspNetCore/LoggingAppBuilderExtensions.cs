3db7d6e450f9d87473e4645f950aa02135804d73
Alexey Zimarev <alex@zimarev.com>
2021-11-19 11:53:11 +0100

    Fixing ambiguous reference

--- src/Core/src/Eventuous.AspNetCore/LoggingAppBuilderExtensions.cs
+++ src/Core/src/Eventuous.AspNetCore/LoggingAppBuilderExtensions.cs
@@ -7,14 +7,7 @@ using Microsoft.Extensions.Logging;
 namespace Eventuous.AspNetCore; 
 
 [PublicAPI]
-public static class LoggingBuilderExtensions {
-    public static IApplicationBuilder AddEventuousLogs(this IApplicationBuilder builder) {
-        if (_listener != null)
-            _listener = new LoggingEventListener(builder.ApplicationServices.GetRequiredService<LoggerFactory>());
-
-        return builder;
-    }
-
+public static class LoggingAppBuilderExtensions {
     public static IHost AddEventuousLogs(this IHost host) {
         if (_listener != null)
             _listener = new LoggingEventListener(host.Services.GetRequiredService<LoggerFactory>());
