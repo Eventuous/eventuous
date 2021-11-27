43f1b786d96fafdf344e1c15fa757bd8c83af1cf
Alexey Zimarev <alex@zimarev.com>
2021-11-27 13:37:11 +0100

    Add discovery experiment

--- src/Extensions/src/Eventuous.AspNetCore.Web/RouteBuilderExtensions.cs
+++ src/Extensions/src/Eventuous.AspNetCore.Web/RouteBuilderExtensions.cs
@@ -1,6 +1,9 @@
+using System.Net.Mime;
+using System.Reflection;
 using Eventuous.AspNetCore.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 // ReSharper disable CheckNamespace
 
@@ -44,9 +47,59 @@ public static class RouteBuilderExtensions {
         );
 
     [PublicAPI]
-    public static ApplicationServiceRouteBuilder<T> MapAggregateCommands<T>(this IEndpointRouteBuilder builder) 
+    public static ApplicationServiceRouteBuilder<T> MapAggregateCommands<T>(this IEndpointRouteBuilder builder)
         where T : Aggregate => new(builder);
-    
+
+    [PublicAPI]
+    public static IEndpointRouteBuilder MapDiscoveredCommands<TAggregate>(
+        this   IEndpointRouteBuilder builder,
+        params Assembly[]            assemblies
+    ) where TAggregate : Aggregate {
+        var assembliesToScan = assemblies.Length == 0
+            ? AppDomain.CurrentDomain.GetAssemblies() : assemblies;
+
+        var attributeType = typeof(HttpCommandAttribute);
+
+        foreach (var assembly in assembliesToScan) {
+            MapAssemblyCommands(assembly);
+        }
+
+        void MapAssemblyCommands(Assembly assembly) {
+            var decoratedTypes = assembly.DefinedTypes.Where(
+                x => x.IsClass && x.CustomAttributes.Any(a => a.AttributeType == attributeType)
+            );
+
+            foreach (var type in decoratedTypes) {
+                var attr = type.GetAttribute<HttpCommandAttribute>()!;
+                Map(type, attr.Route);
+            }
+        }
+
+        void Map(Type type, string route) {
+            builder.MapPost(
+                route,
+                async Task<IResult>(HttpContext context) => {
+                    var cmd = await context.Request.ReadFromJsonAsync(type, context.RequestAborted);
+
+                    if (cmd == null)
+                        throw new InvalidOperationException("Failed to deserialize the command");
+
+                    var service = context.RequestServices.GetRequiredService<IApplicationService<TAggregate>>();
+                    var result  = await service.Handle(cmd, context.RequestAborted);
+
+                    return result.AsResult<TAggregate>();
+                }
+            )
+                .Accepts(type, false, "application/json")
+                .Produces<Result>()
+                .Produces<ErrorResult>(StatusCodes.Status404NotFound)
+                .Produces<ErrorResult>(StatusCodes.Status409Conflict)
+                .Produces<ErrorResult>(StatusCodes.Status400BadRequest);
+        }
+
+        return builder;
+    }
+
     static IResult AsResult<T>(this Result result) where T : Aggregate
         => result is ErrorResult error
             ? error.Exception switch {
@@ -54,7 +107,7 @@ public static class RouteBuilderExtensions {
                 AggregateNotFoundException<T>     => Results.NotFound(error),
                 _                                 => Results.BadRequest(error)
             } : Results.Ok(result);
-    
+
     static T? GetAttribute<T>(this Type type) where T : class => Attribute.GetCustomAttribute(type, typeof(T)) as T;
 }
 
@@ -68,7 +121,7 @@ public class ApplicationServiceRouteBuilder<T> where T : Aggregate {
         _builder.MapCommand<TCommand, T>();
         return this;
     }
-    
+
     public ApplicationServiceRouteBuilder<T> MapCommand<TCommand>(string route) where TCommand : class {
         _builder.MapCommand<TCommand, T>(route);
         return this;
