bb8bf8564c290cbc4660e36ebfbc7fcb0a2cffa3
Alexey Zimarev <alex@zimarev.com>
2022-02-23 22:34:34 +0100

    Trace stream gap collection

--- src/EventStore/src/Eventuous.EventStore/Subscriptions/Diagnostics/StreamSubscriptionMeasure.cs
+++ src/EventStore/src/Eventuous.EventStore/Subscriptions/Diagnostics/StreamSubscriptionMeasure.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using Eventuous.Diagnostics;
 using Eventuous.Subscriptions.Diagnostics;
 
 namespace Eventuous.EventStore.Subscriptions.Diagnostics;
@@ -21,26 +23,37 @@ class StreamSubscriptionMeasure {
     readonly Func<EventPosition?> _getLast;
 
     public async ValueTask<SubscriptionGap> GetSubscriptionGap(CancellationToken cancellationToken) {
-        var read = _eventStoreClient.ReadStreamAsync(
-            Direction.Backwards,
-            _streamName,
-            StreamPosition.End,
-            1,
-            cancellationToken: cancellationToken
-        );
+        using var activity = EventuousDiagnostics.ActivitySource
+            .StartActivity(ActivityKind.Internal)
+            ?.SetTag("stream", _streamName);
 
         try {
+            var read = _eventStoreClient.ReadStreamAsync(
+                Direction.Backwards,
+                _streamName,
+                StreamPosition.End,
+                1,
+                cancellationToken: cancellationToken
+            );
+
             var events = await read.ToArrayAsync(cancellationToken).NoContext();
             var last   = _getLast()?.Position ?? 0;
+
+            activity?.SetActivityStatus(ActivityStatus.Ok());
 
             return new SubscriptionGap(
                 _subscriptionId,
                 events[0].Event.EventNumber - last,
-                DateTime.UtcNow             - events[0].Event.Created
+                DateTime.UtcNow - events[0].Event.Created
             );
         }
         catch (StreamNotFoundException) {
+            activity?.SetActivityStatus(ActivityStatus.Ok());
             return new SubscriptionGap(_subscriptionId, 0, TimeSpan.Zero);
+        }
+        catch (Exception e) {
+            activity?.SetActivityStatus(ActivityStatus.Error(e));
+            throw;
         }
     }
 }
