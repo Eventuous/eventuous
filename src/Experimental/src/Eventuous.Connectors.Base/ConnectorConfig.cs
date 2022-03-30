c475fb334f79c4db796715d4eb8bf4c714cee4db
Alexey Zimarev <alex@zimarev.com>
2022-03-30 22:42:29 +0200

    Fix the connector config

--- src/Experimental/src/Eventuous.Connectors.Base/ConnectorConfig.cs
+++ src/Experimental/src/Eventuous.Connectors.Base/ConnectorConfig.cs
@@ -1,7 +1,11 @@
 namespace Eventuous.Connectors.Base;
 
 public record ConnectorConfig<TSource, TTarget> where TSource : class where TTarget : class {
+    public ConnectorSettings Connector { get; init; } = new();
+    public TSource           Source    { get; init; } = null!;
+    public TTarget           Target    { get; init; } = null!;
+}
+
+public record ConnectorSettings {
     public string  ConnectorId { get; init; } = "default";
-    public TSource Source      { get; init; } = null!;
-    public TTarget Target      { get; init; } = null!;
 }
