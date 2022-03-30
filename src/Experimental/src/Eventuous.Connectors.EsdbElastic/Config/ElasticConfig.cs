d8ee46aef03fb81ae2bcd67a4bec4b746f89f520
Alexey Zimarev <alex@zimarev.com>
2022-03-30 16:37:21 +0200

    Removed Elastic connector to a separate repository

--- src/Experimental/src/Eventuous.Connectors.EsdbElastic/Config/ElasticConfig.cs
+++
@@ -1,9 +0,0 @@
-using Eventuous.Connectors.EsdbElastic.Index;
-
-namespace Eventuous.Connectors.EsdbElastic.Config;
-
-public record ElasticConfig {
-    public string?     ConnectionString { get; init; }
-    public string?     ApiKey           { get; init; }
-    public IndexConfig DataStream       { get; init; } = null!;
-}
--- src/Experimental/src/Eventuous.Connectors.EsdbElastic/Config/EsdbConfig.cs
+++
@@ -1,6 +0,0 @@
-namespace Eventuous.Connectors.EsdbElastic.Config; 
-
-public record EsdbConfig {
-    public string ConnectionString { get; init; } = null!;
-    public uint   ConcurrencyLimit { get; init; } = 1;
-}
