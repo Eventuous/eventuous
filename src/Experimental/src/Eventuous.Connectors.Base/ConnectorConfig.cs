013ce845d3d68501c9639de3088bacbb94027b19
Alexey Zimarev <alex@zimarev.com>
2022-03-31 11:26:01 +0200

    Diagnostic config Connection registration

--- src/Experimental/src/Eventuous.Connectors.Base/ConnectorConfig.cs
+++ src/Experimental/src/Eventuous.Connectors.Base/ConnectorConfig.cs
@@ -7,5 +7,13 @@ public record ConnectorConfig<TSource, TTarget> where TSource : class where TTar
 }
 
 public record ConnectorSettings {
-    public string  ConnectorId { get; init; } = "default";
+    public string            ConnectorId { get; init; } = "default";
+    public DiagnosticsConfig Diagnostics { get; init; } = new();
+}
+
+public record DiagnosticsConfig {
+    public bool Enabled    { get; init; } = true;
+    public bool Trace      { get; init; } = true;
+    public bool Metrics    { get; init; } = true;
+    public bool Prometheus { get; init; } = true;
 }
