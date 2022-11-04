c868200ad8019ad2186d7e7e7b9e68f2b0e08e4e
Alexey Zimarev <alex@zimarev.com>
2022-11-04 12:54:55 +0100

    Fixes #93

--- src/Extensions/src/Eventuous.AspNetCore.Web/ResultExtensions.cs
+++ src/Extensions/src/Eventuous.AspNetCore.Web/ResultExtensions.cs
@@ -1,6 +1,7 @@
+using System.Net;
 using Microsoft.AspNetCore.Http;
 
-namespace Eventuous.AspNetCore.Web; 
+namespace Eventuous.AspNetCore.Web;
 
 public static class ResultExtensions {
     public static IResult AsResult(this Result result)
@@ -8,8 +9,13 @@ public static class ResultExtensions {
             ? error.Exception switch {
                 OptimisticConcurrencyException => Results.Conflict(error),
                 AggregateNotFoundException     => Results.NotFound(error),
-                _                              => Results.BadRequest(error)
+                _ => Results.Problem(
+                    new ProblemDetails {
+                        Status = (int?)HttpStatusCode.InternalServerError,
+                        Title  = error.ErrorMessage,
+                        Detail = error.Exception?.ToString(),
+                        Type = error.Exception?.GetType().Name
+                    }
+                )
             } : Results.Ok(result);
-
-    
 }
