ce697b1b010b076dcb0735a7bb51fbf8bb0715ea
Alexey Zimarev <alex@zimarev.com>
2023-01-05 13:38:46 +0100

    Fixed the constructor params for the checkpoint commit handler (#171)

---
+++ src/Benchmarks/Benchmarks/GapDetectionBenchmarks.cs
@@ -0,0 +1,48 @@
+// Copyright (C) Ubiquitous AS. All rights reserved
+// Licensed under the Apache License, Version 2.0.
+
+using BenchmarkDotNet.Attributes;
+using Eventuous.Subscriptions.Checkpoints;
+using Eventuous.Subscriptions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser]
+public class GapDetectionBenchmarks {
+    int[]                   _numbers = null!;
+    NoOpCheckpointStore     _store   = null!;
+    CheckpointCommitHandler _cch     = null!;
+    LogContext              _log     = null!;
+
+    [GlobalSetup]
+    public void Setup() {
+        _store = new NoOpCheckpointStore();
+
+        _store.CheckpointStored += (sender, checkpoint) => Console.WriteLine(checkpoint);
+
+        var numbers = Enumerable.Range(1, 1000).ToList();
+        numbers.RemoveAll(x => x % 10 == 0);
+        _numbers = numbers.ToArray();
+
+        _log = new LogContext("test", new NullLoggerFactory());
+    }
+
+    [IterationSetup]
+    public void IterationSetup() {
+        _cch = new CheckpointCommitHandler("test", _store, 10);
+    }
+
+    [IterationCleanup]
+    public void IterationCleanup() {
+        _cch.DisposeAsync().GetAwaiter().GetResult();
+    }
+
+    [Benchmark]
+    public async Task CheckpointCommitHandler() {
+        foreach (var number in _numbers) {
+            var p = new CommitPosition((ulong)number, (ulong)number) { LogContext = _log };
+            await _cch.Commit(p, CancellationToken.None);
+        }
+    }
+}
