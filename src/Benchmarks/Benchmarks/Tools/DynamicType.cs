5a8c6c38fb8234c9880ae63b0a9d12517f627c89
Alexey Zimarev <alex@zimarev.com>
2023-01-05 14:13:06 +0100

    Remove the empty docs file prop

--- src/Benchmarks/Benchmarks/Tools/DynamicType.cs
+++ src/Benchmarks/Benchmarks/Tools/DynamicType.cs
@@ -32,6 +32,6 @@ public class DynamicAssembly {
             MethodAttributes.RTSpecialName
         );
 
-        return dynamicType.CreateType();
+        return dynamicType.CreateType()!;
     }
 }
