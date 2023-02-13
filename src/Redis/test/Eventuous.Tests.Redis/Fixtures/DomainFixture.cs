f03139a8efb09c49b80496fbdccc7f0493f4c5ec
Claudiu Chis <claudiuchis76@gmail.com>
2023-02-13 01:02:23 +0000

    Redis store

---
+++ src/Redis/test/Eventuous.Tests.Redis/Fixtures/DomainFixture.cs
@@ -0,0 +1,21 @@
+using Eventuous.Sut.App;
+using MicroElements.AutoFixture.NodaTime;
+using NodaTime;
+using static Eventuous.Tests.Redis.Fixtures.IntegrationFixture;
+
+namespace Eventuous.Tests.Redis.Fixtures;
+
+public static class DomainFixture {
+    static IFixture Auto { get; } = new Fixture().Customize(new NodaTimeCustomization());
+
+    static DomainFixture() => TypeMap.RegisterKnownEventTypes();
+        
+    public static Commands.ImportBooking CreateImportBooking() {
+        var from = Auto.Create<LocalDate>();
+
+        return Auto.Build<Commands.ImportBooking>()
+            .With(x => x.CheckIn, from)
+            .With(x => x.CheckOut, from.PlusDays(2))
+            .Create();
+    }
+}
---
+++ src/Redis/test/Eventuous.Tests.Redis/Store/Helpers.cs
@@ -0,0 +1,42 @@
+using Eventuous.Tests.Redis.Fixtures;
+using static Eventuous.Sut.App.Commands;
+using static Eventuous.Sut.Domain.BookingEvents;
+
+namespace Eventuous.Tests.Redis.Store;
+
+public static class Helpers {
+    public static StreamName GetStreamName()
+        => new(SharedAutoFixture.Auto.Create<string>());
+
+    public static BookingImported CreateEvent()
+        => ToEvent(DomainFixture.CreateImportBooking());
+
+    public static IEnumerable<BookingImported> CreateEvents(int count) {
+        for (var i = 0; i < count; i++) {
+            yield return CreateEvent();
+        }
+    }
+
+    static BookingImported ToEvent(ImportBooking cmd)
+        => new(cmd.RoomId, cmd.Price, cmd.CheckIn, cmd.CheckOut);
+
+    public static Task<AppendEventsResult> AppendEvents(
+        this IEventStore           store,
+        StreamName            stream,
+        object[]              evt,
+        ExpectedStreamVersion version
+    ) {
+        var streamEvents = evt.Select(x => new StreamEvent(Guid.NewGuid(), x, new Metadata(), "", 0));
+        return store.AppendEvents(stream, version, streamEvents.ToArray(), default);
+    }
+
+    public static Task<AppendEventsResult> AppendEvent(
+        this IEventStore      store,
+        StreamName            stream,
+        object                evt,
+        ExpectedStreamVersion version
+    ) {
+        var streamEvent = new StreamEvent(Guid.NewGuid(), evt, new Metadata(), "", 0);
+        return store.AppendEvents(stream, version, new[] { streamEvent }, default);
+    }
+}
