6a81c54f629ef3d981e9d250b081317df3813a29
Alexey Zimarev <alex@zimarev.com>
2023-04-28 14:49:35 +0200

    Add the CLoud Run Pub/Sub endpoint mapping

---
+++ src/GooglePubSub/src/Eventuous.GooglePubSub.CloudRun/EndpointMappingExtensions.cs
@@ -0,0 +1,15 @@
+// Copyright (C) Ubiquitous AS. All rights reserved
+// Licensed under the Apache License, Version 2.0.
+
+using Eventuous.GooglePubSub.CloudRun;
+
+// ReSharper disable CheckNamespace
+
+namespace Microsoft.AspNetCore.Builder;
+
+public static class EndpointMappingExtensions {
+    public static WebApplication MapCloudRunPubSubSubscription(this WebApplication app, string path = "/") {
+        CloudRunPubSubSubscription.MapSubscription(app, path);
+        return app;
+    }
+}
