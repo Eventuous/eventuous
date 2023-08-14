3eee5a59d0e8a26cf2e2f6fbdf55cec617efdeac
Alexey Zimarev <alex@zimarev.com>
2023-08-14 17:16:14 +0200

    Cleanup

--- src/Postgres/src/Eventuous.Postgresql/Extensions/ServiceCollectionExtensions.cs
+++ src/Postgres/src/Eventuous.Postgresql/Extensions/ServiceCollectionExtensions.cs
@@ -15,19 +15,18 @@ public static class ServiceCollectionExtensions {
     ) {
         var options = storeOptions ?? new PostgresStoreOptions();
 
-        services.AddNpgsqlDataSource(
-            connectionString,
-            builder => builder.MapComposite<NewPersistedEvent>(new Schema(options.Schema).StreamMessage)
-        );
+        services.AddNpgsqlDataSource(connectionString, builder => builder.MapComposite<NewPersistedEvent>(new Schema(options.Schema).StreamMessage));
+
         services.AddSingleton<PostgresStore>(
             provider => {
                 var dataSource      = provider.GetRequiredService<NpgsqlDataSource>();
                 var opt             = provider.GetService<IOptions<PostgresStoreOptions>>()?.Value ?? provider.GetService<PostgresStoreOptions>() ?? options;
                 var eventSerializer = provider.GetService<IEventSerializer>();
-                var metaSerializer = provider.GetService<IMetadataSerializer>();
+                var metaSerializer  = provider.GetService<IMetadataSerializer>();
 
                 return new PostgresStore(dataSource, opt, eventSerializer, metaSerializer);
-            });
+            }
+        );
 
         return services;
     }
