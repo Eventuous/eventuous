36299a2e962d976144fe3a88d12e3dcdac416639
Alexey Zimarev <alex@zimarev.com>
2023-11-17 14:52:09 +0100

    Fixing kafka tests (#295)
    * Refactoring subs
    * Add .NET 8 for SUT

---
+++ src/Core/src/Eventuous.Subscriptions/Channels/ChannelWorkerBase.cs
@@ -0,0 +1,39 @@
+// Copyright (C) Ubiquitous AS.All rights reserved
+// Licensed under the Apache License, Version 2.0.
+
+using System.Threading.Channels;
+
+namespace Eventuous.Subscriptions.Channels;
+
+abstract class ChannelWorkerBase<T> : IAsyncDisposable {
+    readonly CancellationTokenSource _cts = new();
+    readonly Task[]                  _readerTasks;
+
+    public Func<CancellationToken, ValueTask>? OnDispose { get; set; }
+
+    public ValueTask Write(T element, CancellationToken cancellationToken)
+        => _stopping ? default : _channel.Write(element, _throwOnFull, cancellationToken);
+
+    bool                _stopping;
+    readonly Channel<T> _channel;
+    readonly bool       _throwOnFull;
+
+    protected ChannelWorkerBase(Channel<T> channel, Func<CancellationToken, Task> processor, int concurrencyLevel, bool throwOnFull = false) {
+        _channel     = channel;
+        _throwOnFull = throwOnFull;
+        _readerTasks = Enumerable.Range(0, concurrencyLevel).Select(x => Task.Run(() => processor(_cts.Token))).ToArray();
+    }
+
+    public async ValueTask DisposeAsync() {
+        _stopping = true;
+        await _channel.Stop(_cts, _readerTasks, OnDispose).NoContext();
+#if NET8_0
+        await _cts.CancelAsync().NoContext();
+#else
+        _cts.Cancel();
+#endif
+        await Task.WhenAll(_readerTasks).NoThrow();
+        _cts.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
