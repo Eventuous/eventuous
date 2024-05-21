1464f84a4d5295dbec19e38c38ef5f58e370297a
Ralph Jansen <ralph.jansen@outlook.com>
2024-05-21 12:23:55 +0200

    Add RegistrationTests for SQL Server

---
+++ src/SqlServer/test/Eventuous.Tests.SqlServer/Registrations/RegistrationTests.cs
@@ -0,0 +1,56 @@
+﻿using Eventuous.Diagnostics.Tracing;
+using Eventuous.SqlServer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eventuous.Tests.SqlServer.Registrations;
+public class RegistrationTests {
+    const string ConnectionString = "Server=localhost;User Id=sqlserver;Password=secret;Database=eventuous;TrustServerCertificate=True";
+
+    [Fact]
+    public void Should_resolve_store_with_manual_registration() {
+        var builder = new WebHostBuilder();
+        builder.Configure(_ => { });
+
+        builder.ConfigureServices(
+            services => {
+                services.AddAggregateStore<SqlServerStore>();
+                services.AddSingleton(new SqlServerStoreOptions() { ConnectionString = ConnectionString });
+            }
+        );
+        var app = builder.Build();
+        var aggregateStore = app.Services.GetRequiredService<IAggregateStore>();
+        aggregateStore.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Should_resolve_store_with_extensions() {
+        var builder = new WebHostBuilder();
+        var config = new Dictionary<string, string?> {
+            ["sqlserver:schema"] = "test",
+            ["sqlserver:connectionString"] = ConnectionString
+        };
+        builder.ConfigureAppConfiguration(cfg => cfg.AddInMemoryCollection(config));
+        builder.Configure(_ => { });
+
+        builder.ConfigureServices(
+            (ctx, services) => {
+                services.AddAggregateStore<SqlServerStore>();
+                services.AddEventuousSqlServer(ctx.Configuration.GetSection("sqlserver"));
+            }
+        );
+        var app            = builder.Build();
+        var aggregateStore = app.Services.GetService<IAggregateStore>();
+        aggregateStore.Should().NotBeNull();
+        var reader       = app.Services.GetService<IEventStore>();
+
+
+
+        // 'TracedEventStore.Inner' is inaccessible due to its protection level...
+        //var store = ((reader as TracedEventStore)!).Inner as SqlServerStore;
+        //store.Should().NotBeNull();
+        //store!.Schema.Should().Be("test");
+    }
+}
