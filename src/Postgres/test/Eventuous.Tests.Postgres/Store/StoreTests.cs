c3d440437be1503b4c6c515ca17e1de036e5445c
Ralph Jansen <ralph.jansen@outlook.com>
2024-05-21 11:26:06 +0200

    Consolidate StoreTests files more with EventStore

--- src/Postgres/test/Eventuous.Tests.Postgres/Store/StoreTests.cs
+++ src/Postgres/test/Eventuous.Tests.Postgres/Store/StoreTests.cs
@@ -1,13 +1,14 @@
 using Eventuous.Tests.Persistence.Base.Store;
+
 // ReSharper disable UnusedType.Global
 
 namespace Eventuous.Tests.Postgres.Store;
 
 [Collection("Database")]
-public class AppendEvents(StoreFixture fixture) : StoreAppendTests<StoreFixture>(fixture);
+public class Append(StoreFixture fixture) : StoreAppendTests<StoreFixture>(fixture);
 
 [Collection("Database")]
-public class ReadEvents(StoreFixture fixture) : StoreReadTests<StoreFixture>(fixture);
+public class Read(StoreFixture fixture) : StoreReadTests<StoreFixture>(fixture);
 
 [Collection("Database")]
 public class OtherMethods(StoreFixture fixture) : StoreOtherOpsTests<StoreFixture>(fixture);
