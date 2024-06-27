22c25c767d05af85e02f5d796d1e80210d08fb43
Alexey Zimarev <alex@zimarev.com>
2024-06-27 19:20:11 +0200

    Fix the meta

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Producion-grade event sourcing and CQRS with .NET">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
