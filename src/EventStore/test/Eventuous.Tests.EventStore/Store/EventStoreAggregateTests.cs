c4e32b527774b73ab147ed23ff1ab14b8754ee89
Alexey Zimarev <alex@zimarev.com>
2024-12-16 15:12:31 +0100

    Remove weird suggestion

--- src/EventStore/test/Eventuous.Tests.EventStore/Store/EventStoreAggregateTests.cs
+++ src/EventStore/test/Eventuous.Tests.EventStore/Store/EventStoreAggregateTests.cs
@@ -18,11 +18,6 @@ public class EventStoreAggregateTests {
         _log           = _loggerFactory.CreateLogger<AggregateStoreTests>();
     }
 
-<<<<<<<<<<<<<<  ✨ Codeium Command ⭐ >>>>>>>>>>>>>>>>
-    /// <summary>
-    /// Verifies that appending an event to an aggregate stream traces the <see cref="DistributedTracing.TraceId"/> and <see cref="DistributedTracing.SpanId"/>.
-    /// </summary>
-<<<<<<<  805dce60-3810-4dbe-8ba7-daa0fbe59742  >>>>>>>
     [Test]
     [Category("Store")]
     public async Task AppendedEventShouldBeTraced(CancellationToken cancellationToken) {
