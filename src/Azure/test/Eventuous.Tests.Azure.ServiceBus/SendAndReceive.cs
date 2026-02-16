6fb6d0390df34a3c5e1069093e463b083abeb062
Alexey Zimarev <alex@zimarev.com>
2026-02-16 21:51:26 +0100

    Add retries to AzureDB tests

--- src/Azure/test/Eventuous.Tests.Azure.ServiceBus/SendAndReceive.cs
+++ src/Azure/test/Eventuous.Tests.Azure.ServiceBus/SendAndReceive.cs
@@ -30,6 +30,7 @@ public class SendAndReceive {
     }
 
     [Test]
+    [Retry(3)]
     public async Task SingleMessage() {
         await _producer.Produce(_streamName, SomeEvent.Create(), _metadata, cancellationToken: TestCancellationToken);
 
@@ -42,6 +43,7 @@ public class SendAndReceive {
     }
 
     [Test]
+    [Retry(3)]
     public async Task LoadsOfMessages() {
         const int count = 200;
 
